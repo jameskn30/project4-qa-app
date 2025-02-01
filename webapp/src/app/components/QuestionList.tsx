cb6b8fa6f2c25ba2f64e220c6c33721307b04999
James <a@gmail.com>
2025-02-01 09:37:56 -0500

    added some colors to the components

--- webapp/src/app/components/QuestionList.tsx
+++ webapp/src/app/components/QuestionList.tsx
@@ -5,8 +5,8 @@ const Question = ({ order, text }: { order: number; text: string }) => {
         <div className="flex items-center justify-between p-2 bg-green-100 rounded-lg shadow mb-4 border border-green-300">
             <span className="text-lg font-bold text-green-700">{order}</span>
             <span className="flex-1 mx-4 text-gray-700">{text}</span>
-            <button className="p-1 bg-green-500 hover:bg-green-700 text-white rounded">
-                <FaAngleUp />
+            <button className="p-1 text-black rounded text-sm">
+                <FaAngleUp className="bg-green-500 p-1 text-white" size={20}/>
                 up
             </button>
         </div>
