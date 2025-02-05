1ad4f8bf02ce226d6b06eafa6adf675100218e05
James Nguyen <jknguyen3010@gmail.com>
2025-02-05 11:08:59 -0500

    updated TODO in chat.ts

--- webapp/src/pages/api/chat.ts
+++ webapp/src/pages/api/chat.ts
@@ -9,6 +9,13 @@ export default function handler(
   res: NextApiResponse<ResponseData>
 ) {
   if (req.method === 'GET' && req.query.roomId) {
+    //TODO: forward request to retrieve the websocket url, 
+    // meant to authorize the websocket URL but if user is authorized once, 
+    // they'll have the  Websocket URL forever, think up a solution to this
+    // currently, the next.js app not really a http-proxy, the client
+    // still connects directly to the fastapi backend anyway
+
+
     const chatApiEndpoint = process.env.CHATAPI_ENDPOINT || 'Not set';
     const roomId = req.query.roomId;
     res.status(200).json({ websocketUrl: `${chatApiEndpoint}/join/${roomId}` });
