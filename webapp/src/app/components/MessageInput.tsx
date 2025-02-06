3be806557bad0d73a35744427a49086efc96d2f4
James Nguyen <jknguyen3010@gmail.com>
2025-02-06 10:19:38 -0500

    worked on the UI a little bit

--- webapp/src/app/components/MessageInput.tsx
+++ webapp/src/app/components/MessageInput.tsx
@@ -1,5 +1,8 @@
 'use client'
 import React, { useState } from 'react';
+import { FaPaperPlane } from "react-icons/fa6";
+import { Input } from "@/components/ui/input"
+
 
 interface MessageInputProps {
     onSent: (message: string) => void;
@@ -24,16 +27,31 @@ const MessageInput = ({onSent} : MessageInputProps) => {
     }
 
     return (
-        <div className="p-4 border-t border-gray-300 flex bg-green-200">
-            <input
+        <div className="p-4 border-t border-gray-300 flex gap-2 items-center justify-stretch">
+            <button 
+                onClick={handleOnSend} 
+                className="p-3 text-white rounded-md
+                hover:ring-2
+                focus:outline-none "
+            >
+                😊
+            </button>
+            <Input
                 type="text"
                 value={message}
                 onChange={handleOnChangeMessage}
                 onKeyDown={handleKeyDown}
-                className="flex-1 p-2 border border-gray-300 rounded-l"
+                className="flex-1 p-1 border border-gray-300 text-sm
+                rounded-lg focus:outline-none focus:ring-2 "
                 placeholder="Type your message..."
             />
-            <button onClick={handleOnSend} className="p-2 bg-green-500 text-white rounded-r">Send</button>
+            <button 
+                onClick={handleOnSend} 
+                className="p-3 bg-indigo-500 text-white rounded-lg hover:bg-indigo-700
+                 focus:outline-none"
+            >
+                <FaPaperPlane/>
+            </button>
         </div>
     )
 }
